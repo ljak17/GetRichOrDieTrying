4b54ee129414c450e6312fb2f718f264f2858a36
Liselotte Jakobsson <liselotte.jakobsson@gmail.com>
2017-12-04 12:01:35 +0100

    New uppdate to Hand by Arasto och Liselotte

--- src/Hand.java
+++ src/Hand.java
@@ -1,5 +1,10 @@
+package GetRichOrDieTrying;
+
 import java.util.LinkedList;
 import java.util.List;
+
+import GetRichOrDieTrying.src.Card;
+import GetRichOrDieTrying.src.PokerHand;
 
 public class Hand {
 
@@ -14,11 +19,11 @@ public class Hand {
 		super();
 		this.cards = cards;
 	}
-	
+
 	public int getSize() {
 		return cards.size();
 	}
-	
+
 	public Card getCard(int index) {
 		return cards.get(index);
 	}
@@ -85,38 +90,112 @@ public class Hand {
 	}
 
 	private boolean isRoyalFlush() {
+		if (cards.get(0).getSuit() == cards.get(4).getSuit()) { //// OBS SORTERA FÄRGEN
+			if (cards.get(0).getValue() == 10) {
+				if (cards.get(1).getValue() == 11 && cards.get(2).getValue() == 12 && cards.get(3).getValue() == 13
+						&& cards.get(4).getValue() == 14) {
+					return true;
+				}
+			}
+		}
+
 		return false;
 	}
 
-	private boolean isStraightFlush() {
+	private boolean isStraightFlush() {/// sortera på färg
+		if (cards.get(0).getSuit() == cards.get(4).getSuit()) {
+			if (cards.get(0).getValue() == cards.get(1).getValue() - 1
+					&& cards.get(1).getValue() == cards.get(2).getValue() - 1
+					&& cards.get(2).getValue() == cards.get(3).getValue() - 1
+					&& cards.get(3).getValue() == cards.get(4).getValue() - 1) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	private boolean isFourOfAKind() {
+		if (cards.get(0).getValue() == cards.get(1).getValue() && cards.get(1).getValue() == cards.get(2).getValue()
+				&& cards.get(2).getValue() == cards.get(3).getValue()) {
+			return true;
+		} else if (cards.get(1).getValue() == cards.get(2).getValue()
+				&& cards.get(2).getValue() == cards.get(3).getValue()
+				&& cards.get(3).getValue() == cards.get(4).getValue()) {
+			return true;
+		}
+
 		return false;
 	}
 
 	private boolean isFullHouse() {
+		if ((cards.get(0).getValue() == cards.get(1).getValue()) && (cards.get(2).getValue() == cards.get(3).getValue()
+				&& cards.get(3).getValue() == cards.get(4).getValue())) {
+			return true;
+		} else if ((cards.get(3).getValue() == cards.get(4).getValue())
+				&& (cards.get(0).getValue() == cards.get(1).getValue()
+						&& cards.get(1).getValue() == cards.get(2).getValue())) {
+			return true;
+		}
 		return false;
 	}
 
-	private boolean isFlush() {
+	private boolean isFlush() {/// Sortera på färg
+		if (cards.get(0).getSuit() == cards.get(4).getSuit()) {
+
+			return true;
+
+		}
 		return false;
 	}
 
 	private boolean isStraight() {
+		if (cards.get(0).getValue() == cards.get(1).getValue() - 1
+				&& cards.get(1).getValue() == cards.get(2).getValue() - 1
+				&& cards.get(2).getValue() == cards.get(3).getValue() - 1
+				&& cards.get(3).getValue() == cards.get(4).getValue() - 1) {
+			return true;
+		}
+
 		return false;
 	}
 
 	private boolean isThreeOfAKind() {
+		if (cards.get(0).getValue() == cards.get(1).getValue() && cards.get(1).getValue() == cards.get(0).getValue()) {
+			return true;
+		} else if (cards.get(1).getValue() == cards.get(2).getValue()
+				&& cards.get(2).getValue() == cards.get(3).getValue()) {
+			return true;
+		} else if (cards.get(2).getValue() == cards.get(3).getValue()
+				&& cards.get(3).getValue() == cards.get(4).getValue()) {
+			return true;
+		}
+
 		return false;
 	}
 
 	private boolean isTwoPair() {
+		if (cards.get(0).getValue() == cards.get(1).getValue() && cards.get(3).getValue() == cards.get(4).getValue()) {
+			return true;
+		} else if (cards.get(1).getValue() == cards.get(2).getValue()
+				&& cards.get(3).getValue() == cards.get(4).getValue()) {
+			return true;
+		} else if (cards.get(0).getValue() == cards.get(1).getValue()
+				&& cards.get(2).getValue() == cards.get(3).getValue()) {
+			return true;
+		}
 		return false;
 	}
 
 	private boolean isRoyalPair() {
+		if (cards.get(0).getValue() == cards.get(1).getValue() && cards.get(0).getValue() >= 11) {
+			return true;
+		} else if (cards.get(1).getValue() == cards.get(2).getValue() && cards.get(1).getValue() >= 11) {
+			return true;
+		} else if (cards.get(2).getValue() == cards.get(3).getValue() && cards.get(2).getValue() >= 11) {
+			return true;
+		} else if (cards.get(3).getValue() == cards.get(4).getValue() && cards.get(3).getValue() >= 11) {
+			return true;
+		}
 		return false;
 	}
 
